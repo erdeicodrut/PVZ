6251d636106756fb570b852f789f0d1db389626e
Codrut Erdei <codrut@hackermail.com>
2017-04-10 20:00:34 +0300

    well, we fixed shit

--- Plant.js
+++ Plant.js
@@ -1,10 +1,11 @@
 class Plant extends Movable {
-    constructor(type) {
-        this.effect = Effect.NONE;
-        // this.bulletFactory = new BulletFactory(type);
+    constructor(type, x, y, width, height, hp) {
+    	this.speed = 10;
+    	super(x, y, width, height, hp);
+        this.effect = type; //Effect.NONE;
     }
 
     shoot() {
-        return new Bullet(this.x, this.y, 10, this.damage); //TODO get rid of magic numbers
+        return new Bullet(this.x, this.y, this.speed, this.damage, this.effect);
     }
 }
