4dd34b820ccdcaf6acb2f5d51ad6efe8764a11e1
coddy <codrut@hackermail.com>
2017-05-11 19:28:01 +0300

    Well, All done

---
+++ level.py
@@ -0,0 +1,22 @@
+import io
+
+file = io.open('src/enemies.txt', mode="w")
+
+print("How many Simple Zombies do you want?")
+simpleZombie = input()
+
+print("How many Zombies with flags do you want?")
+flagZombie = input()
+
+print("How many Zombies with buckets on their heads do you want?")
+bucketHead = input()
+
+print("How many Zombies that jump do you want?")
+paulVault = input()
+
+print("How many Zombies with cons on their heads do you want?")
+conHead = input()
+
+file.write('SimpleZombie:{} FlagZombie:{} BucketHeadZombie:{} PoleVaultingZombie:{} ConeHeadZombie:{}'.format(simpleZombie, flagZombie, bucketHead, paulVault, conHead))
+
+file.close()
